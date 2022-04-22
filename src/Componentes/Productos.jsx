09fd1a3309eeacafb72b440eb0addd8af578c7a2
Fabio <fabioramosnic@gmail.com>
2022-04-22 15:00:45 -0300

    agregado de producto funcionando

--- src/Componentes/Productos.jsx
+++ src/Componentes/Productos.jsx
@@ -1,9 +1,23 @@
 import './Productos.css'
 import React from 'react'
-import { Button, Card, Dropdown, Form } from 'react-bootstrap'
-import CardProductos from './Llamados/CardProductos'
+import { Button, Card, Dropdown, Form, NavLink } from 'react-bootstrap'
+import axios from "axios";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect, useState } from "react";
 
 export default function Productos() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+      const productos = async () => {
+          const { data } = await axios.get("/productos");
+          setProducts(data);
+      };
+      productos()
+
+      Aos.init({ duration: 1000 });
+  }, []);
   return (
     <div>
       <div className='Productos-inicio'>
@@ -29,7 +43,36 @@ export default function Productos() {
           </div>
         </div>
         <div className='Todos-productos'>
-          {/* <CardProductos /> */}
+        <div className="Cards-productos">
+                {products.map((prod) => {
+                    return (prod && (
+                        <NavLink
+                            key={prod._id}
+                            style={{ textDecorationLine: "none" }}
+                            to={`/individual/${prod._id}`}
+                            exact
+                            as={NavLink}
+                        >
+                            <div data-aos="fade-up" className="CardP-style">
+                                <Card className="card-cont2">
+                                    <Card.Body>
+                                        <div className="Cardp-imgcont">
+                                            <img style={{ objectFit: "cover" }} className="Cardp-img" src={prod.img[0]} alt="" />
+                                        </div>
+                                        <Card.Title>{prod.nombre}</Card.Title>
+                                        <Card.Text className="module line-clamp">
+                                            {prod.descripcion}
+                                        </Card.Text>
+                                        <Card.Text className="module line-clamp">
+                                            {prod.price}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </NavLink>)
+                    );
+                })}
+            </div>
         </div>
       </div>
     </div>
